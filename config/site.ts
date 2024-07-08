c5baec76214a16c2aa537892ef2af47c4ee739df
Muhammad Ihsan <emhihsan@gmail.com>
2024-07-08 16:27:16 +0700

    finish step 3: Site Header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig ={
+    name: "Example Blog",
+    url: "https://example.com",
+    description: "NextJs blog",
+    author: "Muhammad Ihsan",
+    links:{
+        twitter: "https://twitter.com/emhaihsan",
+        github: "https://github.com/emhaihsan",
+        personalSite: "https://ihsan.id"
+    }
+}
+
+export type SiteConfig = typeof siteConfig
